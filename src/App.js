c2940a08bd717c5631e5c98a1e626b6f94f7e00e
yuriikovalchuk <jimbokid89@gmail.com>
2023-01-04 18:44:28 +0200

    feat - basic functionality

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 import {PALLETE_OPTIONS} from "./constants/appConstants";
 import Typography from "@mui/material/Typography";
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const App = () => {
   const [settings, setSettings] = useState({
@@ -46,42 +47,41 @@ const App = () => {
     p5ref.current.sketch.saveCanvas('Generated Canvas', 'png')
   }
 
-  return (
-    <Paper variant="outlined" sx={{p: {xs: 2, md: 3}}}>
-      <Typography variant="h3" gutterBottom>
-        Generative Art Generator
-      </Typography>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={4}>
-          <Settings
-            settings={settings}
-            setSettings={setSettings}
-            drawCanvas={drawCanvas}
-          />
+  return (<Paper variant="outlined" sx={{p: {xs: 2, md: 3}}}>
+    <Typography variant="h3" gutterBottom >
+      <a href="https://github.com/jimbokid" target={'_blank'}>
+        <GitHubIcon fontSize="large" color="primary"/>
+      </a>
 
-        </Grid>
-        <Grid item xs={12} md={8}>
-          <div className={'canvasWrapper'}>
-            <Button variant="outlined" type="button" onClick={() => {
-              saveCanvas()
-            }}
-                    style={{
-                      marginBottom: 10
-                    }}
-            >Download </Button>
-            <Sketch setup={setup} draw={(p5) => draw(p5, settings)} ref={p5ref} style={{
-              width: '100%',
-              height: '100%'
-            }}/>
-          </div>
-        </Grid>
+      Generative Art Generator
+    </Typography>
+    <Grid container spacing={2}>
+      <Grid item xs={12} md={4}>
+        <Settings
+          settings={settings}
+          setSettings={setSettings}
+          drawCanvas={drawCanvas}
+        />
+
       </Grid>
+      <Grid item xs={12} md={8}>
+        <div className={'canvasWrapper'}>
+          <Button variant="outlined" type="button" onClick={() => {
+            saveCanvas()
+          }}
+                  style={{
+                    marginBottom: 10
+                  }}
+          >Download </Button>
+          <Sketch setup={setup} draw={(p5) => draw(p5, settings)} ref={p5ref} style={{
+            width: '100%', height: '100%'
+          }}/>
+        </div>
+      </Grid>
+    </Grid>
 
-      <Typography variant="overline" display="block" gutterBottom>
-        <a href="https://github.com/jimbokid" target={'_blank'}>My github</a>
-      </Typography>
-    </Paper>
-  );
+
+  </Paper>);
 };
 
 export default App;
